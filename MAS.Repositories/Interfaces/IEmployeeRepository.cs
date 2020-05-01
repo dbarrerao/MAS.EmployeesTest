72dab9d23c6fec7ba0a29e3c408e6c552dda976a
Diego Alejandro Barrera Ocampo <dbarrerao@gmail.com>
2020-05-01 11:35:38 -0500

    1. Addition conectionString to API in appsettings.json 2. Addition method in IEmployeeRepository for Get all employees 3. Consume Api and implement method for Get all employees

---
+++ MAS.Repositories/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,13 @@
+﻿using MAS.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAS.Repositories.Interfaces
+{
+    public interface IEmployeeRepository
+    {
+        Task<List<Employee>> GetAllEmployees();
+    }
+}
---
+++ MAS.Repositories/Repositories/EmployeeRepository.cs
@@ -0,0 +1,37 @@
+﻿using MAS.Models;
+using MAS.Repositories.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAS.Repositories.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private IConfiguration _configuration;
+        private readonly HttpClient HttpClient;
+        HttpResponseMessage _response;
+
+        public EmployeeRepository()
+        {
+            HttpClient = new HttpClient()
+            {
+                BaseAddress = new Uri(_configuration.GetSection("ConectionString:EmployeesAPi").Value.ToString())
+            };
+        }
+
+        async Task<List<Employee>> IEmployeeRepository.GetAllEmployees()
+        {
+            _response = await HttpClient.GetAsync("Employees");
+            var result = await _response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<List<Employee>>(result);
+        }
+
+    }
+}
+
+
