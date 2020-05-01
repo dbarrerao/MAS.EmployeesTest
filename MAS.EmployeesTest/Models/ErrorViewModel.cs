c0fac22c1389ed39765dbdd1a8cabf56a095b2ba
Diego Alejandro Barrera Ocampo <dbarrerao@gmail.com>
2020-05-01 10:21:39 -0500

    Project Creation (NetCore 2.1) and MVC

---
+++ MAS.EmployeesTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MAS.EmployeesTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
